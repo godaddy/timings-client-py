204216965c51edebe14f973c927f3995c171c393
Marcel Verkerk <mverkerk@godaddy.com>
2018-05-31 16:37:04 -0700

    Update release number

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     author='GoDaddy Operating Company, LLC',
     author_email='mverkerk@godaddy.com',
     license='MIT',
-    version='1.0.2',
+    version='1.0.3',
     install_requires=required_packages_list('requirements.txt'),
     packages=find_packages(
         exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']))
